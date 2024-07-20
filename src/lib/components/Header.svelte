94cc127a805e1bdb60b1edd4e816ab9a29a87259
Antonio S. Dromundo <asdromundo@asdrome.com>
2024-07-19 23:08:44 -0600

    Start Blog Example

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import * as config from '$lib/config';
+	import DarkmodeButton from './DarkmodeButton.svelte';
+</script>
+
+<nav>
+	<!-- Title -->
+	<a href="/" class="title">
+		<b>{config.title}</b>
+	</a>
+
+	<!-- Navigation -->
+	<ul class="links">
+		<li>
+			<a href="/about">About</a>
+		</li>
+		<li>
+			<a href="/contact">Contact</a>
+		</li>
+		<li>
+			<a href="/rss.xml" target="_blank">RSS</a>
+		</li>
+	</ul>
+
+	<!-- Theme -->
+	<DarkmodeButton />
+</nav>
