0b6b43bd2d408fc4b7d9f7180fa721646ce53738
Antonio S. Dromundo <asdromundo@asdrome.com>
2024-07-19 20:43:18 -0600

    Set to be an static Svelte 5 site

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,13 +6,23 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	compilerOptions: {
+		runes: true
+	},
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
 	}
+	//  Uncomment to disable runes in node_modules
+	/* 	vitePlugin: {
+			dynamicCompileOptions({ filename }) {
+				if (filename.includes('node_modules')) {
+					return { runes: undefined } // or false, check what works
+				}
+			}
+		} */
 };
 
 export default config;
