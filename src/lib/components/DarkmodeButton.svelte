47e7bcdc09047946a6498aedc9aff30552f56f08
Antonio S. Dromundo <asdromundo@asdrome.com>
2024-07-20 00:21:10 -0600

    Fixed Header and DarkMode

--- src/lib/components/DarkmodeButton.svelte
+++ src/lib/components/DarkmodeButton.svelte
@@ -3,4 +3,18 @@
 	import Moon from 'svelte-radix/Moon.svelte';
 
 	import { resetMode, setMode } from 'mode-watcher';
+	import { DropdownMenu } from 'bits-ui';
+	import type { Icon } from 'lucide-svelte';
+	import { SunMoon } from 'lucide-svelte';
 </script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		<SunMoon />
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content sideOffset={8}>
+		<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
